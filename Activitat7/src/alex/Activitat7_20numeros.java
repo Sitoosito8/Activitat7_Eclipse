af5c525835295d23d323eec75c8ffdc09168bb03
Alex Lopez España <a20000lex@gmail.com>
2021-01-17 21:46:54 +0100

    Segundo commit

--- Activitat7/src/alex/Activitat7_20numeros.java
+++ Activitat7/src/alex/Activitat7_20numeros.java
@@ -2,7 +2,7 @@ package alex;
 public class Activitat7_20numeros {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 25; i++) {
 			System.out.println(" numero  "+(i+1));
 		}
 
